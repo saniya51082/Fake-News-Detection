39248b611a5cea9db685d56ed9afb094c74b1c2d
saniya <saniya51082@gmail.com>
2026-04-10 09:51:23 +0100

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 vectorizer = joblib.load("vectorizer.jb")
 model = joblib.load("lr_model.jb")
 
-st.title("Fake News Detector")
+st.title("Fake News Detection App")
 st.write("Enter a News Article below to check whether it is Fake or Real")
 
 news_input = st.text_area("News Article:","")
